e6425dabccabc53b5a448b18b1fff1cb9ef20747
Jack Morris <jack.morris10@gmail.com>
2016-03-05 20:16:10 +0000

    Added Ex01-09

---
+++ Ex01-09/main.go
@@ -0,0 +1,37 @@
+// Exercise 1.9: Print the content of each URL passed as an argument to stdout, adding http if not given.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+
+		// Prepend protocal if not given.
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
+
+		// Fetch the contents of the URL into resp.
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		// Print the response code
+		fmt.Printf("Response code: %v\n", resp.Status)
+
+		// Copy the body of resp into stdout.
+		_, err = io.Copy(os.Stdout, resp.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+	}
+}
